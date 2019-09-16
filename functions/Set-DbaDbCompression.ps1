600ad5bf9f9b551fed86fe50d84c3798b0124930
ggoncz <dev@gaborgoncz.com>
2019-09-16 12:07:00 +0100

    add table filter parameter

--- functions/Set-DbaDbCompression.ps1
+++ functions/Set-DbaDbCompression.ps1
@@ -110,6 +110,7 @@ function Set-DbaDbCompression {
         [int]$MaxRunTime = 0,
         [int]$PercentCompression = 0,
         $InputObject,
+        [string]$TableFilter = "*",
         [switch]$EnableException
     )
 
@@ -155,8 +156,8 @@ function Set-DbaDbCompression {
                             Write-Message -Level Verbose -Message "Using passed in compression suggestions"
                             $compressionSuggestion = $InputObject | Where-Object { $_.Database -eq $db.name }
                         } else {
-                            Write-Message -Level Verbose -Message "Testing database for compression suggestions for $instance.$db"
-                            $compressionSuggestion = Test-DbaDbCompression -SqlInstance $server -Database $db.Name
+                            Write-Message -Level Verbose -Message "Testing database for compression suggestions for $instance.$db $($TableFilter.Replace('*','%'))"
+                            $compressionSuggestion = Test-DbaDbCompression -SqlInstance $server -Database $db.Name -TableFilter $($TableFilter.Replace('*','%'))
                         }
                     }
                 } catch {
@@ -192,7 +193,7 @@ function Set-DbaDbCompression {
                     } else {
                         if ($Pscmdlet.ShouldProcess($db, "Applying $CompressionType compression")) {
                             Write-Message -Level Verbose -Message "Applying $CompressionType compression to all objects in $($db.name)"
-                            foreach ($obj in $server.Databases[$($db.name)].Tables | Where-Object { !$_.IsMemoryOptimized -and !$_.HasSparseColumn }) {
+                            foreach ($obj in $server.Databases[$($db.name)].Tables | Where-Object { !$_.IsMemoryOptimized -and !$_.HasSparseColumn -and $_.Name -like $TableFilter }) {
                                 if ($MaxRunTime -ne 0 -and ($(Get-Date) - $starttime).TotalMinutes -ge $MaxRunTime) {
                                     Write-Message -Level Verbose -Message "Reached max run time of $MaxRunTime"
                                     break
